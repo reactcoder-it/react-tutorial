e0bae817bf968b8e9539df2720dca7e099e78a4a
Vadim Pashaev <vmp@live.ru>
2018-10-06 19:39:34 +0400

    Updates

---
+++ precache-manifest.1fdf7bba8da02aa5cfdf2ca25d2ddb4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "817a331b1ec5619e79d9",
+    "url": "/react-tutorial/static/js/main.817a331b.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/react-tutorial/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "817a331b1ec5619e79d9",
+    "url": "/react-tutorial/static/css/main.b4ebfd95.chunk.css"
+  },
+  {
+    "revision": "e80401d0afdc946612020d5a53d14663",
+    "url": "/react-tutorial/index.html"
+  }
+];
